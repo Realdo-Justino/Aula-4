9c62dce9e17fb571e7fb62638201d926596ee177
unknown <realdojj@gmail.com>
2020-08-26 18:31:48 -0300

    exercicio-28

---
+++ Aula04-28.py
@@ -0,0 +1,6 @@
+primeiroNumero=float(input("Insira o primeiro numero "))
+segundoNumero=float(input("Insira o primeiro numero "))
+terceiroNumero=float(input("Insira o primeiro numero "))
+soma=((primeiroNumero**2)+(segundoNumero**2)+(terceiroNumero**2))
+
+print("A soma dos quadrados dos numeros é",soma)
